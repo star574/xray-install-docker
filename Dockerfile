12167893e9fc5bc1522440378294f0f21553cd2b
star574 <shihengluo574@gmail.com>
2021-11-26 13:50:20 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM nginx
+LABEL author=star574
+WORKDIR /root/
+ENV DOMAIN ""
+ENV UUID ""
+ENV PATH ""
+COPY config.json /usr/local/etc/xray/config.json
+COPY web.conf /etc/nginx/conf.d/web.conf
+COPY run.sh /root/run.sh
+VOLUME  /usr/local/etc/xray
+VOLUME  /etc/nginx
+VOLUME  /usr/share/nginx/html
+VOLUME  /usr/share/nginx/logs
+EXPOSE 80
+EXPOSE 443
+RUN bash ./run.sh
+CMD nginx -g 'daemon off;'
---
+++ run.sh
@@ -0,0 +1,25 @@
+!#/bin/bash
+# xray
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root \
+    && bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --without-geodata \
+systemctl enable --now xray \
+
+# html
+git clone https://github.com/star574/camouflage_html.git \
+mv camouflage_html/* /usr/share/nginx/html/ \
+rm -rf camouflage_html
+
+# acme.sh ssl
+curl  https://get.acme.sh | sh -s email=my@example.com
+source ~/.bashrc
+mkdir -p  /etc/nginx/conf/ssl/${DOMAIN}
+acme.sh  --issue  -d ${DOMAIN} --nginx
+acme.sh --install-cert -d www.springcloud.tk   \
+--key-file       /etc/nginx/conf/ssl/${DOMAIN}/${DOMAIN}.key  \
+--fullchain-file /etc/nginx/conf/ssl/${DOMAIN}/fullchain.pem \
+--reloadcmd     "service nginx force-reload"
+
+
+# etc
+mv /etc/nginx/conf/conf.d/web.conf /etc/nginx/conf/conf.d/${DOMAIN}.conf
+mv /etc/nginx/conf/conf.d/default.conf /etc/nginx/conf/conf.d/default.conf.bak
